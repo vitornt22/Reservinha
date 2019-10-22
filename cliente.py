1222cc2b9804866afb03332f9a2c503e6ac094f8
RayllaVitoria <raylla.vitoria.37@gmail.com>
2019-10-22 08:58:34 -0300

    Banco de Dados Completo

--- cliente.py
+++ cliente.py
@@ -1,7 +1,7 @@
 import socket
 class cliente(object):
     def __init__(self,dados):
-        ip = '192.168.0.125'
+        ip = '10.180.42.149'
         port = 7000
         addr = ((ip,port)) #define a tupla de endereco
         client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM) #AF_INET parametro para informar a familia do protocolo, SOCK_STREAM indica que eh TCP/IP
