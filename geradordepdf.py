e11152445d8b75c8a64662de7624bbe9a4bb6ffe
FULLRAIVA <j-maria-lima2015@bol.com.br>
2019-10-28 16:48:40 -0200

    PDF formatado

--- geradordepdf.py
+++ geradordepdf.py
@@ -1,9 +1,20 @@
 from reportlab.pdfgen import canvas
+from reportlab.platypus import Image
+
 class GeraPDF(object):
     def __init__(self,nome_arquivo):
         pdf = canvas.Canvas(nome_arquivo+".PDF")
-        pdf.drawString(0, 800, "O Monitor vai ficar na sala de 12 até as 14 horas")
+        img = 'Ufpi_brasão.jpg'
+        pdf.setFont("Helvetica",20)
+        pdf.drawString(150, 800, "UNIVERSIDADE FEDERAL DO PÍAUI")
+        pdf.setFont("Helvetica",12)
+        pdf.drawImage(img,200,550,width=200,height=200)
+        pdf.drawString(0, 500, "O Seguinte pessoa está habilitada a utilizar a sala 809:")
+        pdf.drawString(300,500, "José Maria dos Santos Leal CPF: 123.123.123")
+        
+       
+       
         pdf.save()
 
-
-
+ 
+teste = GeraPDF("FORMAT")
